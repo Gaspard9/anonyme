268eacc9c75d3c6050dfd4afc7d2441ada190928
Gaspard9 <gaspard.vercamer@gmail.com>
2024-10-02 18:48:53 +0400

    codingal project

--- question.py
+++ question.py
@@ -1,4 +1,2 @@
-a=int(input("enter a number in kg to convert it to g:"))
-print("the number is converted to grams",a*1000)
-b=int(input("enter a number in m to convert it to cm:"))
-print("the number is converted to cm",b*100)
+a=int(input("enter a number to multiply it by itself: "))
+print(a*a)
